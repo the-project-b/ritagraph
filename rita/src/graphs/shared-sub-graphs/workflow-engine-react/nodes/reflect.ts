e5d385281d0f276f09df2dabc894f3d666f9decd
Pascal Lohscheidt <pascal@project-b.dev>
2025-07-03 13:31:10 +0200

    fix: attempt to fix the issue - experimental push

--- rita/src/graphs/shared-sub-graphs/workflow-engine-react/nodes/reflect.ts
+++ rita/src/graphs/shared-sub-graphs/workflow-engine-react/nodes/reflect.ts
@@ -1,7 +1,11 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
 import { z } from "zod";
-import { AIMessageChunk, HumanMessage } from "@langchain/core/messages";
+import {
+  AIMessageChunk,
+  HumanMessage,
+  SystemMessage,
+} from "@langchain/core/messages";
 import { WorkflowEngineNode, WorkflowEngineStateType } from "../sub-graph.js";
 
 export const reflect: WorkflowEngineNode = async (state) => {
@@ -13,19 +17,22 @@ export const reflect: WorkflowEngineNode = async (state) => {
 
   const llm = new ChatOpenAI({ model: "gpt-4o-mini", temperature: 0.1 });
 
-  const systemPrompt = PromptTemplate.fromTemplate(`
+  const systemPrompt = await PromptTemplate.fromTemplate(
+    `
 Your job is to be a Payroll Specialist and your counterpart has come up with a plan and called some tools
 Check if the information collected is enough to solve the users request.
 Don't be too strict and don't ask for information that the user has not asked for unless it is obviously missing.
 If not reflect on what information is missing or what is required to solve the users request.
+If the agent says its unable to find or provide the information then ACCEPT.
 
 Respond in JSON format with the following fields:
 - decision: ACCEPT or IMPROVE
 - reflection: The reflection on the task if decision is IMPROVE
-`);
+`
+  ).format({});
 
   const chatPrompt = await ChatPromptTemplate.fromMessages([
-    ["system", await systemPrompt.format({})],
+    new SystemMessage(systemPrompt),
     lastUserMessage,
     ...state.taskEngineMessages, //todo safely slice last 5 messages
   ]).invoke({});
