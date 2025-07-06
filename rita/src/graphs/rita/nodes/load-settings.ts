a4e240c63e921074cb509c6ca33c04e16ccc0578
Pascal Lohscheidt <pascal@project-b.dev>
2025-07-06 23:00:36 +0200

    fix: reverted changes about language settings (#18)

--- rita/src/graphs/rita/nodes/load-settings.ts
+++ rita/src/graphs/rita/nodes/load-settings.ts
@@ -5,13 +5,12 @@ import { Node } from "../graph-state.js";
  * Responsible for initializing the settings for the graph.
  */
 export const loadSettings: Node = async (state, config) => {
-  const user = getAuthUser(config);
+  const { user } = getAuthUser(config);
 
   return {
-    preferredLanguage:
-      state.preferredLanguage ?? user.user.preferredLanguage ?? "DE",
+    preferredLanguage: user.preferredLanguage ?? "DE",
     // Just for development we are using a backup company id based on the config
     selectedCompanyId:
-      state.selectedCompanyId ?? user.user.company.id ?? config.backupCompanyId,
+      state.selectedCompanyId ?? user.company.id ?? config.backupCompanyId,
   };
 };
