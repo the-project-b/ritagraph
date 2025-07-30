a7bcfd92b0bdfed764790f9daaab92812d600489
Pascal Lohscheidt <pascal@project-b.dev>
2025-07-30 17:37:44 +0200

    fix: great catch from andries
    Co-Authored by: andries-project-b<andries@project-b.dev>

--- rita/src/utils/graphql/client.ts
+++ rita/src/utils/graphql/client.ts
@@ -2,11 +2,14 @@ import { GraphQLClient } from "graphql-request";
 import { getSdk } from "../../generated/graphql";
 
 export function createGraphQLClient(accessToken: string) {
-  const client = new GraphQLClient("http://localhost:3001/graphqlapi/schema", {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+  const client = new GraphQLClient(
+    `${process.env.PROJECTB_GRAPHQL_ENDPOINT}/schema`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
   const sdk = getSdk(client);
 
   return sdk;
