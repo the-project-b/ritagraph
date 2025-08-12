79ce02fa2c3ec9bf2ca6858cfdca63967a4cfe37
andries-project-b <andries@project-b.dev>
2025-08-12 11:10:23 +0200

    fix: loadsettings

--- packages/rita-graphs/src/graphs/rita/nodes/load-settings.ts
+++ packages/rita-graphs/src/graphs/rita/nodes/load-settings.ts
@@ -9,15 +9,18 @@ type AssumedConfigurableType = {
  * Responsible for initializing the settings for the graph.
  */
 export const loadSettings: Node = async (state, config, getAuthUser) => {
-  const { user, token } = getAuthUser(config);
+  const { user, token, appdataHeader } = getAuthUser(config);
   const { backupCompanyId } = getContextFromConfig(config);
   const { thread_id } =
     config.configurable as unknown as AssumedConfigurableType;
 
+  const selectedCompanyId =
+    state.selectedCompanyId ?? user.company.id ?? backupCompanyId;
+
   await appendMessageAsThreadItem({
     message: state.messages.at(-1),
     langgraphThreadId: thread_id,
-    accessToken: token,
+    ctx: { accessToken: token, selectedCompanyId, appdataHeader },
   });
 
   return {
