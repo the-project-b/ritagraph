2a3f204986d81bc28a556ca6bab6d82e642173c3
andries-project-b <andries@project-b.dev>
2025-08-27 12:41:39 +0200

    feat: setting up for automated testing

---
+++ packages/prompts/jest.config.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.(ts|js)',
+    '**/*.(test|spec).(ts|js)'
+  ],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: false,
+      tsconfig: {
+        module: 'commonjs',
+        target: 'es2020'
+      }
+    }],
+    '^.+\\.js$': ['ts-jest', {
+      useESM: false
+    }]
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@the-project-b)/)'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.(ts|js)',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+    '!src/**/*.spec.ts',
+    '!src/**/*.test.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true
+};
